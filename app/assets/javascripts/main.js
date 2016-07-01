cf4ea76331d1f27a5605bbb7af1e73ffd7974ca7
hyungccho <hyungchulcho93@gmail.com>
2016-06-30 19:54:33 -0700

    test this on staging

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -43,7 +43,7 @@ function isValidEmail(email) {
 
 function toggleLoader (bool) {
   if (bool) {
-    $('.submit').stop(true, false).addClass('loading', { duration: 500 });
+    $('.submit').stop(true, false).addClass('loading');
     $('.submit').stop(true, false).removeClass('fa fa-plus', { duration: 500 })
   } else {
     $('.submit').removeClass('loading').addClass('fa fa-plus')
